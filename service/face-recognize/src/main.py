f444247d76feac43445663a512664ba4d66f4f20
MuhammadFelda <ifelfelda27@gmail.com>
2026-02-06 14:40:07 +0700

    Major change at main

--- service/face-recognize/src/main.py
+++ service/face-recognize/src/main.py
@@ -1,41 +1,18 @@
-import cv2
-import sys
-import os
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.registration import router as registration_router
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from model import initialize_model, process_frame, load_face
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-def main():
-    print("Sedang memuat model AI...")
-    model_ai = initialize_model()
-    
-    print("Sedang mendaftarkan wajah dari folder data/faces...")
-    load_face(model_ai)
-    
-    cap = cv2.VideoCapture(0)
-    
-    if not cap.isOpened():
-        print("Error: Kamera tidak dapat diakses!")
-        return
-    
-    print("Sistem Berjalan. Tekan 'q' pada jendela gambar untuk keluar.")
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Gagal mengambil gambar dari kamera.")
-            break
-
-        processed_frame = process_frame(frame, model_ai)
-
-        cv2.imshow("Face Recognition Testing - Local Mode", processed_frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    print("Sistem dihentikan.")
+app.include_router(registration_router)
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
