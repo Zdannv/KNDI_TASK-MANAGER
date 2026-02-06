6a844da6f9a29806490223ca01c4b4721026b7b5
MuhammadFelda <ifelfelda27@gmail.com>
2026-02-06 14:35:41 +0700

    Added field face_embedding

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,8 @@ class User extends Authenticatable
         'email',
         'role',
         'password',
-        'avatar', // <--- TAMBAHKAN INI
+        'avatar',
+        'face_embedding',
     ];
 
     /**
@@ -36,6 +37,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'face_embedding',
     ];
 
     /**
@@ -48,6 +50,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'face_embedding' => 'array',
         ];
     }
 
