ce1f81ec5b7bf3c44233ad3b283748f22f96d55b
MuhammadFelda <ifelfelda27@gmail.com>
2026-02-10 15:24:35 +0700

    Added column check_in_confidence & check_out_confidence

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -9,7 +9,9 @@ class Attendance extends Model
     protected $fillable = [
         'user_id',
         'check_in_time',
+        'check_in_confidence',
         'check_out_time',
+        'check_out_confidence',
     ];
 
     public function user()
