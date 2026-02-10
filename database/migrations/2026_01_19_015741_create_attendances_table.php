e58a382e363cf0e3a0fdd76f55be3cb52621d17a
MuhammadFelda <ifelfelda27@gmail.com>
2026-02-10 15:26:17 +0700

    Added column check_in_confidence & check_out_confidence

--- database/migrations/2026_01_19_015741_create_attendances_table.php
+++ database/migrations/2026_01_19_015741_create_attendances_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->dateTime('check_in_time');
+            $table->decimal('check_in_confidence');
             $table->dateTime('check_out_time')->nullable();
+            $table->decimal('check_out_confidence')->nullable();
             $table->timestamps();
         });
     }
