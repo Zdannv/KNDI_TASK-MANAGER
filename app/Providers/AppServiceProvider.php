783f6fee0da76981bb4c6b2b92a5b46ad62741e0
Muhammad Zaidan Zhafiz Satrianto <zdannfoo@gmail.com>
2026-02-12 06:50:27 +0000

    Revert "deploy config: fix port conflict and add ssl support"
    This reverts commit 6c7395fd0fbef18c84b67d317e8dd7cf85a24731.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // Baris ini yang tadi kurang
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,11 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Memaksa Laravel menggunakan HTTPS jika bukan di local
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
-
         Vite::prefetch(concurrency: 3);
     }
 }
