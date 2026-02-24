f7238253cc3e07533f603517f42a7c0e8be061e4
MuhammadFelda <ifelfelda27@gmail.com>
2026-02-24 13:03:32 +0700

    Covert from icon svg to call icon component

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -2,6 +2,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import Trash from '@/Components/Icon/Trash.vue';
 import { data } from 'autoprefixer';
+import Pen from '@/Components/Icon/Pen.vue';
 
 // 1. Layout Persistent
 export default { layout: AuthenticatedLayout };
@@ -318,10 +319,11 @@ const formatRole = (role) => {
                   </td>
                   <td class="p-5 align-middle">
                     <div class="flex gap-3 justify-center items-center text-sm">
-                      <button @click="handleEdit(user.id)" class="p-1.5 rounded-lg text-primary-600 dark:text-primary-400 hover:bg-primary-50 dark:hover:bg-primary-900/30 transition tooltip-trigger" title="Edit">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                          </svg>
+                      <button 
+                        @click="handleEdit(user.id)" 
+                        class="p-1.5 rounded-lg text-amber-600 dark:text-amber-400 hover:bg-primary-50 dark:hover:bg-primary-900/30 transition tooltip-trigger" title="Edit"
+                      >
+                          <Pen class="w-5 h-5" />
                       </button>
                       <button 
                         @click.prevent="openDeleteModal(user)" 
