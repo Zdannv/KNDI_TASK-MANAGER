5ca2b679aa12fd52d9643fb50d4234d1c30899f8
zaidan <zdannfoo@gmail.com>
2026-03-03 08:45:33 +0700

    add: bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleRequest::class,
+            'check.attendance' => \App\Http\Middleware\CheckAttendanceStatus::class, 
         ]);
 
         $middleware->redirectGuestsTo(fn () => route('welcome'));
