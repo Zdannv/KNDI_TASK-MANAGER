fd63ca1fb9b7c5e3dd9edf7a8473888e2b239565
zaidan <zdannfoo@gmail.com>
2026-03-04 10:27:44 +0700

    add: config for display skill

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,7 @@ Route::post('/attendance/toggle', [AttendanceController::class, 'toggleStatus'])
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/skills', [ProfileController::class, 'updateSkills'])->name('profile.skills.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
